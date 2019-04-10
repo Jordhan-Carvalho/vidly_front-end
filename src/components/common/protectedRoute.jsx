650dd73b1f010924537f307a8f041d45ddd94c08
Jordhan Carvalho <jordhan.rdz@gmail.com>
2019-04-10 10:30:59 -0300

    Initial commit

---
+++ src/components/common/protectedRoute.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+import auth from "../../services/authService";
+
+const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
+  return (
+    <Route
+      path={path}
+      {...rest}
+      render={props =>
+        auth.getCurrentUser() ? (
+          Component ? (
+            <Component {...props} />
+          ) : (
+            render(props)
+          )
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import auth from "../services/authService";
+
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {}
+  };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
+  doSubmit = async () => {
+    const { username, password } = this.state.data;
+
+    try {
+      await auth.login(username, password);
+      // have to do a fully reload of the page so the navbar is updated
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/";
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = error.response.data;
+        this.setState({ errors });
+      }
+    }
+  };
+
+  render() {
+    if (auth.getCurrentUser()) return <Redirect to="/" />;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
